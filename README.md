5622b5f038b980ae07253b2fe6bb300a73bb7e5d
Des Cho <d.chongarova@gmail.com>
2014-03-06 21:42:25 +0200

    Readme update; Conditional including of the post thumbnail on the page template.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-laconical
+Laconical
 =========
 
-The WordPress theme for Dessy Chongarova's personal website chongarova.com
+The WordPress theme for [Dessy Chongarova](http://chongarova.com)'s personal website. Minimal and simple, built from scratch, including only what's needed.
--- page.php
+++ page.php
@@ -1,15 +1,17 @@
 <?php get_header(); ?>
-    <?php while ( have_posts() ) : the_post(); ?>
-    	<article class="entry">
-	    	<header class="page-header">
-	    		<h2 class="entry-title"><?php the_title(); ?></h2>
-    		</header>
-            <div class="entry-thumbnail">
-                <?php the_post_thumbnail(); ?>
-            </div>
-    		<div class="entry-content">
-    			<?php the_content(); ?>
-    		</div>
-    	</article>
-    <?php endwhile; ?>
-    <?php get_footer(); ?>
+	<?php while ( have_posts() ) : the_post(); ?>
+		<article class="entry">
+			<header class="page-header">
+				<h2 class="entry-title"><?php the_title(); ?></h2>
+			</header>
+			<?php if ( has_post_thumbnail() ) : ?>
+			<div class="entry-thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</div>
+			<?php endif; ?>
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+	<?php endwhile; ?>
+	<?php get_footer(); ?>
